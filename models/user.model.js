6ac75b192827cde72b821a72a2f94f58027f25a5
Nazmul Hossain <najmulh62@gmail.com>
2022-09-12 01:14:36 +0600

    Passport Local Session Tutorial

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        unique: true
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
